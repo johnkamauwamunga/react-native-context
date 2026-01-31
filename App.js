251fb11c278cf98a8415a4581306c3b9992f0e2b
johnkamauwamunga <johnkamauwamunga@gmail.com>
2026-01-31 05:01:21 +0300

    innitial commit

---
+++ App.js
@@ -0,0 +1,10 @@
+import { Text, View } from "react-native";
+const App = () => {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>Hello, World!</Text>
+    </View>
+  );
+};
+
+export default App;
